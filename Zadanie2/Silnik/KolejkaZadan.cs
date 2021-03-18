e0ba1840e94c5b479a2f33cbf442d7f9bff32066
szymon-drzewiecki <249478@student.pwr.edu.pl>
2021-03-18 19:34:41 +0100

    dodanie klas

---
+++ Zadanie2/Silnik/KolejkaZadan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silnik
+{
+    class KolejkaZadan
+    {
+        public List<Zadanie> listaZadan;
+
+        public KolejkaZadan()
+        {
+            this.listaZadan = new List<Zadanie>();
+        }
+    }
+}
---
+++ Zadanie2/Silnik/Zadanie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silnik
+{
+    public class Zadanie
+    {
+        public int czasTrwania { get; set; }
+        public int numerZadania { get; set; }
+
+        public Zadanie(int y, int x)
+        {
+            this.czasTrwania = y;
+            this.numerZadania = x;
+        }
+    }
+}
