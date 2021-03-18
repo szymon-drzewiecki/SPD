410de2c4f61a1d7de96d02b6b41dcd88055635f3
Jakub Dudziński <jdudzinski101@gmail.com>
2021-03-18 19:07:39 +0100

    Poprawiono zły commit

--- Zadanie2/Silnik/Properties/AssemblyInfo.cs
+++ Zadanie2/Silnik/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Engine")]
+[assembly: AssemblyTitle("Silnik")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Engine")]
+[assembly: AssemblyProduct("Silnik")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7334678c-ac0d-4bf0-bc5d-c1fb49efa824")]
+[assembly: Guid("636fc9e7-ff45-4d91-a6c2-2eb6a0b2d988")]
 
 // Version information for an assembly consists of the following four values:
 //
