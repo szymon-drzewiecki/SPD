98a4a8d922d99d0e892df5afa5c644dd4bb03017
Jan Binkowski <jan.binkowski@wp.pl>
2021-05-12 17:37:18 +0200

    Charts & time measuring added.

--- Zadanie5/schrage.py
+++ Zadanie5/schrage.py
@@ -1,124 +1,218 @@
-import random
-import operator
 import copy
-import re
-import numpy as np
-import operator
-import copy
+import math
+import time
+import pandas as pd
+from matplotlib import pyplot as plt
+from classes import *
 
-#GLOBAL variables
-task_matrix = 0
-#heap
-heap = 0
-n = 0
 
-"""
-Function used to load data from file.
-path - path to the file
-i_instance - number of wanted instance (starting from 0) 
-"""
 def load_data(path, i_instance):
     _counter = 0
+    _t_counter = 1
     _n_tasks = 0
-    _matrix = []
+    tg = TaskGroup()
     with open(path, 'r') as f:
         for n, line in enumerate(f):
             line = line.replace('\n', '')
             _rpqs = [int(i) for i in line.split(' ')
-                        if i != '']
+                     if i != '']
             if _rpqs:
-               if len(_rpqs) == 3 and _counter == i_instance+1:
-                   _matrix.append(_rpqs)
-               elif len(_rpqs) < 3:
-                   _counter += 1
-                   
-    global task_matrix
-    global heap
-    heap = np.empty(len(_matrix), dtype=int)
-    task_matrix = np.array(_matrix)
-    return _matrix
+                if len(_rpqs) == 3 and _counter == i_instance + 1:
+                    tg.add(_t_counter, Task(_rpqs))
+                    _t_counter += 1
+                elif len(_rpqs) < 3:
+                    _counter += 1
 
-"""
-Function for calculating Cmax from sorted array
-pi_array - list of tasks ordered in the best sequence
-"""
-def calculate_cmax(pi_list):
-    S = pi_list[0][0]
-    C = S + pi_list[0][1]
-    Cmax = C + pi_list[0][2]
-    for i in range(1, len(pi_list)):
-        S = max(pi_list[i][0], C)
-        C = S + pi_list[i][1]
-        Cmax = max(Cmax, C + pi_list[i][2])
+    return tg
 
-    return Cmax
 
-"""
-Function used for pushing element into heap
-"""
-def heap_push(d):
-    global n
-    global heap
-    i = n
-    n += 1
-    j = (i - 1) // 2
+# SCHRAGE
+def schrage(tg):
+    start = time.perf_counter()
 
-    while (i > 0 and heap[j] < d):
-        heap[i] = heap[j]
-        i = j
-        j = (i - 1) // 2
-
-    heap[i] = d
-
-"""
-Function used to remove the root of the heap
-"""
-def heap_pop():
-    global n
-    global heap
-
-    n -= 1
-    if n == 0:
-        v = heap[n]
-        i = 0
-        j = 0
-
-        while (j < n):
-            if (j+1 < n) and (heap[j+1] > heap[j]):
-                j += 1
-            if (v >= heap[j]):
-                break
-            heap[i] = heap[j]
-            i = j
-            j = 2 * j + 1
-
-        heap[i] = v
-
-def schrage(tasks):
-    pi = []
-    G = []
-    N = copy.deepcopy(tasks)
-    t = min(N)[0]
-    while (len(G) != 0 or len(N) != 0):
-        while(len(N) != 0 and min(N)[0] <= t):
-            j = N.index(min(N))
-            G.append(N.pop(j))
-        if len(G) != 0:
-            j = G.index(max(G, key=operator.itemgetter(2)))
-            temporary = G.pop(j)
-            pi.append(temporary)
-            t += temporary[1]
+    pi = TaskGroup()
+    Ng = TaskGroup()
+    Nn = copy.deepcopy(tg)
+    t = min(Nn.get_R().values())
+    while len(Ng.group) != 0 or len(Nn.group) != 0:
+        while len(Nn.group) != 0 and min(Nn.get_R().values()) <= t:
+            j = min(Nn.get_R(), key=Nn.get_R().get)
+            Ng.add(j, Nn.pop(j))
+        if len(Ng.group) == 0:
+            t = min(Nn.get_R().values())
         else:
-            t = min(N)[0]
-    return pi
+            j = max(Ng.get_Q(), key=Ng.get_Q().get)
+            t = t + Ng.group[j].p
+            pi.add(j, Ng.pop(j))
 
-#MAIN FUNCTION
+    Cmax = pi.cmax()
+
+    stop = time.perf_counter()
+    schrageTime = (stop - start)
+
+    return schrageTime, Cmax, pi
+
+
+# SCHRAGE PMTN
+def schrage_pmtn(tg):
+    start = time.perf_counter()
+
+    Cmax = 0
+    tgc = copy.deepcopy(tg)
+    Ng = TaskGroup()
+    Nn = copy.deepcopy(tg)
+    t = 0
+    l = 1
+    while len(Ng.group) != 0 or len(Nn.group) != 0:
+        while len(Nn.group) != 0 and min(Nn.get_R().values()) <= t:
+            j = min(Nn.get_R(), key=Nn.get_R().get)
+            Ng.add(j, Nn.pop(j))
+            if Ng.group[j].q > tgc.group[l].q:
+                tgc.group[l].p = t - Ng.group[j].r
+                t = Ng.group[j].r
+                if tgc.group[l].p > 0:
+                    Ng.add(l, tgc.group[l])
+        if len(Ng.group) == 0:
+            t = min(Nn.get_R().values())
+        else:
+            j = max(Ng.get_Q(), key=Ng.get_Q().get)
+            l = j
+            t = t + Ng.group[j].p
+            Cmax = max(Cmax, t + Ng.group[j].q)
+            Ng.pop(j)
+
+    stop = time.perf_counter()
+    schragePtmnTime = (stop - start)
+
+    return schragePtmnTime, Cmax
+
+
+# SCHRAGE [HEAP]
+def hschrage(tg):
+    start = time.perf_counter()
+
+    pi = TaskGroup()
+    Ng = Heap_MaxQ(len(tg.group))
+    Nn = Heap_MinR(len(tg.group))
+    Nn.tg_import(tg)
+    t = Nn.root().r
+    x = 0
+    while Ng.n != 0 or Nn.n != 0:
+        while Nn.n != 0 and Nn.root().r <= t:
+            j = Nn.root().nr
+            Ng.push(Nn.pop())
+        if Ng.n == 0:
+            t = Nn.root().r
+        else:
+            j = Ng.root().nr
+            t = t + Ng.data[0].p
+            pi.add(j, Task(Ng.pop().rpq()))
+    HCmax = pi.cmax()
+
+    stop = time.perf_counter()
+    hschrageTime = (stop - start)
+
+    return hschrageTime, HCmax, pi
+
+
+# SCHRAGE PMTN [HEAP]
+def hschrage_pmtn(tg):
+    start = time.perf_counter()
+
+    Cmax = 0
+    tgc = copy.deepcopy(tg)
+    Ng = Heap_MaxQ(len(tg.group))
+    Nn = Heap_MinR(len(tg.group))
+    Nn.tg_import(tg)
+    t = 0
+    l = 1
+    while Ng.n != 0 or Nn.n != 0:
+        while Nn.n != 0 and Nn.root().r <= t:
+            j = Nn.root().nr
+            Ng.push(Nn.pop())
+            if Ng.root().q > tgc.group[l].q:
+                tgc.group[l].p = t - Ng.root().r
+                t = Ng.root().r
+                if tgc.group[l].p > 0:
+                    Ng.push(HTask(l, tgc.group[l].rpq()))
+        if Ng.n == 0:
+            t = Nn.root().r
+        else:
+            j = Ng.root().nr
+            l = j
+            t = t + Ng.root().p
+            Cmax = max(Cmax, t + Ng.root().q)
+            Ng.pop()
+    stop = time.perf_counter()
+    hschragePtmnTime = (stop - start)
+
+    return hschragePtmnTime, Cmax
+
+
+# MAIN FUNCTION
 def main():
-    load_data("schrage_data.txt", 0)
-    global task_matrix
-    print(calculate_cmax(schrage(task_matrix.tolist())))
-    print("Finished...")
+    tg50 = load_data("in50.txt", 0)
+    tg100 = load_data("in100.txt", 0)
+    tg200 = load_data("in200.txt", 0)
 
-#AVOIDING RUNNING CODE WHILE IMPORTING THIS MODULE
+    schrageTime50, Cmax50, pi50 = schrage(tg50)
+    schragePtmnTime50, cmax_pmtn50 = schrage_pmtn(tg50)
+    hschrageTime50, HCmax50, hpi50 = hschrage(tg50)
+    hschragePtmnTime50, hcmax_pmtn50 = hschrage_pmtn(tg50)
+    schrageTime100, Cmax100, pi100 = schrage(tg100)
+    schragePtmnTime100, cmax_pmtn100 = schrage_pmtn(tg100)
+    hschrageTime100, HCmax100, hpi100 = hschrage(tg100)
+    hschragePtmnTime100, hcmax_pmtn100 = hschrage_pmtn(tg100)
+    schrageTime200, Cmax200, pi200 = schrage(tg200)
+    schragePtmnTime200, cmax_pmtn200 = schrage_pmtn(tg200)
+    hschrageTime200, HCmax200, hpi200 = hschrage(tg200)
+    hschragePtmnTime200, hcmax_pmtn200 = hschrage_pmtn(tg200)
+    print("\n50 zadan:\n")
+    print("Cmax:", Cmax50, "\ttime execution:", schrageTime50)
+    print("Cmax pmtn:", cmax_pmtn50, "\ttime execution:", schragePtmnTime50)
+    print("H Cmax:", HCmax50, "\ttime execution:", hschrageTime50)
+    print("H Cmax pmtn:", hcmax_pmtn50, "\ttime execution:", hschragePtmnTime50)
+    print("-" * 50)
+    #-------------------------------------------------
+    print("\n100 zadan:\n")
+    print("Cmax:", Cmax100, "\ttime execution:", schrageTime100)
+    print("Cmax pmtn:", cmax_pmtn100, "\ttime execution:", schragePtmnTime100)
+    print("H Cmax:", HCmax100, "\ttime execution:", hschrageTime100)
+    print("H Cmax pmtn:", hcmax_pmtn100, "\ttime execution:", hschragePtmnTime100)
+    print("-" * 50)
+    # -------------------------------------------------
+    print("\n200 zadan:\n")
+    print("Cmax:", Cmax200, "\ttime execution:", schrageTime200)
+    print("Cmax pmtn:", cmax_pmtn200, "\ttime execution:", schragePtmnTime200)
+    print("H Cmax:", HCmax200, "\ttime execution:", hschrageTime200)
+    print("H Cmax pmtn:", hcmax_pmtn200, "\ttime execution:", hschragePtmnTime200)
+    print("-" * 50)
+    # -------------------------------------------------
+    plot1 = plt.figure(1)
+    x = ["50","100","200"]
+    y = [schrageTime50,schrageTime100,schrageTime200]
+    z = [hschrageTime50, hschrageTime100, hschrageTime200]
+    plt.plot(x, y)
+    plt.plot(x, z)
+    plt.title("Czasy wykonywania się algorytmu Schrage")
+    plt.xlabel("Liczba zadań")
+    plt.ylabel("Czas")
+    plt.legend(["Zwykły Schrage","Schrage z kopcem"])
+    # -------------------------------------------------
+    plot2 = plt.figure(2)
+    x1 = ["50", "100", "200"]
+    y1 = [schragePtmnTime50, schragePtmnTime100, schragePtmnTime200]
+    z1 = [hschragePtmnTime50, hschragePtmnTime100, hschragePtmnTime200]
+    plt.plot(x1, y1)
+    plt.plot(x1, z1)
+    plt.title("Czasy wykonywania się algorytmu Schrage z przerwaniami")
+    plt.xlabel("Liczba zadań")
+    plt.ylabel("Czas")
+    plt.legend(["Schrage z przewaniami", "Schrage z przerwaniami z kopcem"])
+    plt.show()
+    plt.show()
+    print("\nScript finished...")
+
+# AVOIDING RUNNING CODE WHILE IMPORTING THIS MODULE
 if __name__ == '__main__':
     main()
