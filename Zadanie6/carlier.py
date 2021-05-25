58775d3169d4650e222a30f2f5c2787be590fd21
Jan Binkowski <jan.binkowski@wp.pl>
2021-05-25 17:58:17 +0200

    Zaimplementowany pseudokod z dobrymi wynikami

--- Zadanie6/carlier.py
+++ Zadanie6/carlier.py
@@ -1,42 +1,51 @@
 from classes import *
 from schrage import *
 
+_counter = 0
+
 def carlier(tg, UB):
-    pi_g = 0
-    U, pi = hschrage(tg)
-    if U < UB:
-        UB = U
-        pi_g = pi
+    global _counter
+    _counter += 1
+    UB, pi = hschrage(tg)
+
+    print(_counter,". węzeł... Cmax=",pi.cmax(),sep='')
 
     b = pi.find_b()
     a = pi.find_a(b)
     c = pi.find_c(a, b)
     if c == None:
-        return pi_g
-    K = pi.get_K(c,b)
+        return pi
+    K = pi.get_K(c, b)
     _tmp = pi.group[c].r
     pi.group[c].r = max(pi.group[c].r, K[0] + K[1])
     LB = hschrage_pmtn(pi)
     Kc = pi.get_K(c, b, with_c=True)
     LB = max(sum(K), sum(Kc), LB)
     if LB < UB:
-        pi_g = carlier(pi, UB)
+        x = carlier(copy.deepcopy(pi), UB)
+        if x.cmax() < UB:
+            pi = x
+            UB = x.cmax()
     pi.group[c].r = _tmp
     _tmp = pi.group[c].q
     pi.group[c].q = max(pi.group[c].q, K[1] + K[2])
     LB = hschrage_pmtn(pi)
     LB = max(sum(K), sum(Kc), LB)
     if LB < UB:
-        pi_g = carlier(pi, UB)
+        x = carlier(copy.deepcopy(pi), UB)
+        if x.cmax() < UB:
+            pi = x
+            UB = x.cmax()
     pi.group[c].q = _tmp
 
+    return pi
 
-    return pi_g
 
 def main():
-    tg = load_data('schrage_data.txt', 10)
+    tg = load_data('schrage_data.txt', 4)
     pi = carlier(tg, math.inf)
     print("Cmax:", pi.cmax())
-    
+
+
 if __name__ == '__main__':
     main()
