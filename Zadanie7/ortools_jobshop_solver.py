1ce08fd22c50477a8e790af1ddc87a2e86710f01
szymon-drzewiecki <249478@student.pwr.edu.pl>
2021-06-09 17:48:19 +0200

    rpq - wczytywanie z rpq.data działa

---
+++ Zadanie7/ortools_jobshop_solver.py
@@ -0,0 +1,132 @@
+import collections
+import copy
+# Import Python wrapper for or-tools CP-SAT solver.
+from ortools.sat.python import cp_model
+
+
+def load_jobshop_data(path, instance):
+    _counter = 0
+    _t_counter = 1
+    tasks = []
+    sublist = []
+    with open(path, 'r') as f:
+        for n, line in enumerate(f):
+            line = line.replace('\n', '')
+            _pwds = [int(i) for i in line.split(' ') if i != '']
+            if _pwds:
+                if len(_pwds) == 3 and _counter == instance + 1:
+                    emptyList = copy.deepcopy(sublist)
+                    emptyList.append((0,_pwds[0]))
+                    emptyList.append((1,_pwds[1]))
+                    emptyList.append((2,_pwds[2]))
+                    tasks.append(emptyList)
+                    _t_counter += 1
+                elif len(_pwds) < 3:
+                    _counter += 1
+
+    return tasks
+
+def MinimalJobshopSat():
+
+
+    """Minimal jobshop problem."""
+    # Create the model.
+    model = cp_model.CpModel()
+
+    jobs_data = load_jobshop_data('rpq.data.txt', 1)
+    print (jobs_data)
+    machines_count = 1 + max(task[0] for job in jobs_data for task in job)
+    all_machines = range(machines_count)
+
+    # Computes horizon dynamically as the sum of all durations.
+    horizon = sum(task[1] for job in jobs_data for task in job) + 1
+
+    # Named tuple to store information about created variables.
+    task_type = collections.namedtuple('task_type', 'start end interval')
+    # Named tuple to manipulate solution information.
+    assigned_task_type = collections.namedtuple('assigned_task_type',
+                                                'start job index duration')
+
+    # Creates job intervals and add to the corresponding machine lists.
+    all_tasks = {}
+    machine_to_intervals = collections.defaultdict(list)
+
+    for job_id, job in enumerate(jobs_data):
+        for task_id, task in enumerate(job):
+            machine = task[0]
+            duration = task[1]
+            suffix = '_%i_%i' % (job_id, task_id)
+            start_var = model.NewIntVar(0, horizon, 'start' + suffix)
+            end_var = model.NewIntVar(0, horizon, 'end' + suffix)
+            interval_var = model.NewIntervalVar(start_var, duration, end_var,
+                                                'interval' + suffix)
+            all_tasks[job_id, task_id] = task_type(start=start_var,
+                                                   end=end_var,
+                                                   interval=interval_var)
+            machine_to_intervals[machine].append(interval_var)
+
+    # Create and add disjunctive constraints.
+    for machine in all_machines:
+        model.AddNoOverlap(machine_to_intervals[machine])
+
+    # Precedences inside a job.
+    for job_id, job in enumerate(jobs_data):
+        for task_id in range(len(job) - 1):
+            model.Add(all_tasks[job_id, task_id +
+                                1].start >= all_tasks[job_id, task_id].end)
+
+    # Makespan objective.
+    obj_var = model.NewIntVar(0, horizon, 'makespan')
+    model.AddMaxEquality(obj_var, [
+        all_tasks[job_id, len(job) - 1].end
+        for job_id, job in enumerate(jobs_data)
+    ])
+    model.Minimize(obj_var)
+
+    # Solve model.
+    solver = cp_model.CpSolver()
+    status = solver.Solve(model)
+
+    if status == cp_model.OPTIMAL:
+        # Create one list of assigned tasks per machine.
+        assigned_jobs = collections.defaultdict(list)
+        for job_id, job in enumerate(jobs_data):
+            for task_id, task in enumerate(job):
+                machine = task[0]
+                assigned_jobs[machine].append(
+                    assigned_task_type(start=solver.Value(
+                        all_tasks[job_id, task_id].start),
+                                       job=job_id,
+                                       index=task_id,
+                                       duration=task[1]))
+
+        # Create per machine output lines.
+        output = ''
+        for machine in all_machines:
+            # Sort by starting time.
+            assigned_jobs[machine].sort()
+            sol_line_tasks = 'Machine ' + str(machine) + ': '
+            sol_line = '           '
+
+            for assigned_task in assigned_jobs[machine]:
+                name = 'job_%i_%i' % (assigned_task.job, assigned_task.index)
+                # Add spaces to output to align columns.
+                sol_line_tasks += '%-10s' % name
+
+                start = assigned_task.start
+                duration = assigned_task.duration
+                sol_tmp = '[%i,%i]' % (start, start + duration)
+                # Add spaces to output to align columns.
+                sol_line += '%-10s' % sol_tmp
+
+            sol_line += '\n'
+            sol_line_tasks += '\n'
+            output += sol_line_tasks
+            output += sol_line
+
+        # Finally print the solution found.
+        print('Optimal Schedule Length: %i' % solver.ObjectiveValue())
+        print(output)
+
+
+MinimalJobshopSat()
